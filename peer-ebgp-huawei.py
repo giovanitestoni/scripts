3a64fea60ed2e3c4ebe85a23fd63a672cb661b69
giovanitestoni <giovanitestoni@outlook.com>
2020-10-02 15:20:34 -0300

    Update peer-ebgp-huawei.py

--- peer-ebgp-huawei.py
+++ peer-ebgp-huawei.py
@@ -1,3 +1,4 @@
+
 print("Gerador de script peer bgp VRP Huawei")
 print()
 input("Pressione Enter para iniciar: \n")
@@ -20,29 +21,3 @@ print('peer '+group+' route-policy IN-IPv4-'+group+' import')
 print('peer '+group+' route-policy OUT-IPv4-'+group+' export')
 print('#')
 print()
-retorno = input('Deseja gerar gerar outro peer? 1-SIM 2-NAO ')
-sim = "1"
-não = "2"
-if retorno == sim:
-    asn_remote = input('Qual o número do ASN remoto? ')
-    group = input('Qual o nome do seu peer group? ')
-    peer = input('Qual o endereco do peer? ')
-    print()
-    print('#')
-    print('route-policy IN-IPv4-'+group +' deny node 1') #Cria filtro negando tudo
-    print('route-policy OUT-IPv4-'+group +' deny node 1') #Cria filtro negando tudo
-    print('#')
-    print("bgp "+asn_local)
-    print('group '+group+' external')
-    print('peer '+group+' as-number '+asn_remote)
-    print('peer '+group+' advertise-community')
-    print('peer '+peer+' group '+group)
-    print('peer '+group+' route-policy IN-IPv4-'+group+' import')
-    print('peer '+group+' route-policy OUT-IPv4-'+group+' export')
-    print('#')
-    print()
-    pass
-if retorno == não:
-    exit()
-    pass
-    
